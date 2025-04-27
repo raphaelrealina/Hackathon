bd08a7a4fcbcdbe5b40f7ec2009c054f1b397a2e
raphaelrealina <155679935+raphaelrealina@users.noreply.github.com>
2025-04-26 23:39:06 -0500

    s

--- Game/Assets/MineableCoal.cs
+++ Game/Assets/MineableCoal.cs
@@ -6,16 +6,19 @@ public class MineableCoal : MonoBehaviour
 {
     public int hitsToBreak = 3;
     private int currentHits = 0;
-    private void OnCollisionEnter(Collision collision)
+    private void onCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Pickaxe"))
         {
             currentHits++;
-            Debug.Log("Coal Hit: hits:" + currentHits);
+
+            Debug.Log("Coal Hit! hits: " + currentHits);
+
             if (currentHits >= hitsToBreak)
             {
                 Destroy(gameObject);
             }
         }
+        Debug.Log("entered function");
     }
 }
